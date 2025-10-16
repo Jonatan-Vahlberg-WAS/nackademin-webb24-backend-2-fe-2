db6d3b360f613e43f63af0ee6e041578ebfdc58b
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-10-16 11:54:34 +0200

    add updation logic

--- src/components/courses/UpdateCourse.tsx
+++ src/components/courses/UpdateCourse.tsx
@@ -1,15 +1,28 @@
 "use client";
 
+import CourseService from "@/utils/courseService";
 import CourseForm from "./CourseForm";
+import { useRouter } from "next/navigation";
 
 type UpdateCourseProps = {
     course: Course;
 }
 
 export default function UpdateCourse({course}: UpdateCourseProps) {
+    const router = useRouter()
 
-    const onUpdate = () => {
-        console.log("Updating course")
+    const onUpdate = async (courseData: Partial<Course> ) => {
+        try {
+      const response = await new CourseService().updateCourse(course.course_id, courseData)
+      if(!response.ok){
+        throw new Error("Course could not be Updated")
+      }
+      const updatedCourse: Course = await response.json()
+      router.refresh()
+
+    } catch (err) {
+      console.warn("Error in creating course", err)
+    }
     }
 
     return (
